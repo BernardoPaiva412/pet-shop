547347ee41faab22fade4ef097fc24a23e0311b5
BernardoPaiva412 <bernardo.apiva@aluno.ifsp.edu.br>
2026-03-11 08:39:46 -0300

    iniciando view

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,7 +1,13 @@
 import { Request, Response } from 'express';
 
 export const home = (req: Request, res: Response) => {
-    res.send('home');
+    // res.send('home');
+    res.render('pages/page'), {
+        banner: {
+            title: 'Todos os animais',
+            background: 'allanimals.jpg'
+        }
+    }
 };
 
 export const dogs = (req: Request, res: Response) => {
